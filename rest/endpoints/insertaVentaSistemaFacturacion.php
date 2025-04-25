88f04b0794d06b59b6e696a112c86c2e91028a39
neutrix94 <oscar_mdp10@hotmail.com>
2025-04-25 17:52:23 -0600

    Se modifica servicio para insertar venta en sistema de facturacion con el tipo de pago que le corresponde 1.2

--- rest/endpoints/insertaVentaSistemaFacturacion.php
+++ rest/endpoints/insertaVentaSistemaFacturacion.php
@@ -113,13 +113,13 @@
         $payment_type = 1;//efectivo por default
         try{
             $sql = "SELECT
-                        id_forma_pago
+                        id_tipo_pago
                     FROM ec_cajero_cobros
                     WHERE id_pedido = {$sale_header['id_pedido']}";
             $stm = $link->query($sql);
             if($stm->rowCount() == 1){
                 $row = $stm->fetch(PDO::FETCH_ASSOC);
-                $payment_type = $row['id_forma_pago'];
+                $payment_type = ($row['id_forma_pago'] == 1 ? 1 : ($row['id_forma_pago'] == 8 ? 9 : 14) );
             }else if($stm->rowCount() > 1){
                 $payment_type = 17;
             }
