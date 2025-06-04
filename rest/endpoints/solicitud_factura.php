bc73cc1a81bf1fcfe8f778d1773dd4f8d08d395c
neutrix94 <oscar_mdp10@hotmail.com>
2025-06-04 12:29:02 -0600

    actualizacion de metodo de pago en genreacion de factura RS

--- rest/endpoints/solicitud_factura.php
+++ rest/endpoints/solicitud_factura.php
@@ -54,9 +54,9 @@
             $RS_resp = curl_exec($crl);//envia peticion
             curl_close($crl);
             $json_resp = json_decode( $RS_resp );
-error_log( "URL : {$bill_api_path}/rest/inserta_venta_sistema_facturacion");
-error_log( "POST DATA : {$post_data}");
-error_log( "Respuesta al subir nota en efectivo : {$RS_resp}");
+//error_log( "URL : {$bill_api_path}/rest/inserta_venta_sistema_facturacion");
+//error_log( "POST DATA : {$post_data}");
+//error_log( "Respuesta al subir nota en efectivo : {$RS_resp}");
         }
     //consulta si ya tiene una solicitud de factura 
         $id_solicitud_factura = null;
@@ -127,7 +127,8 @@ error_log( "Respuesta al subir nota en efectivo : {$RS_resp}");
         $post_data = json_encode( array( 
             "sale_folio"=>$sale_folio, 
             "costumer_rfc"=>$sale_costumer,
-            "cfdi_use"=>$cfdi_use
+            "cfdi_use"=>$cfdi_use,
+            "payment_type"=>$payment_type
         ) );
         //echo "{$api_path}/inserta_venta";
         //public function sendPetition( $url, $post_data ){
