2f9bcbe25d548599b7536ec13979d7404809990c
neutrix94 <oscar_mdp10@hotmail.com>
2025-07-15 13:07:33 -0600

    Version Oscar 2025-07-15 para validar que la factura que se solicita no sea de otro mes 1.1

--- rest/endpoints/buscaVentasPorFolio.php
+++ rest/endpoints/buscaVentasPorFolio.php
@@ -32,19 +32,7 @@
     //validacion de diferencia de meses 
         if($sale['months_difference'] > 0){
             $response->getBody()->write(json_encode( array( "status"=>"200", "was_found"=>"invalid_month", 
-            "message"=>"<div class=\"text-center\">
-                <h3 class=\"text-center\">Lo sentimos</h3>
-                <h5>Su solicitud ha sido rechazada que que la venta '{$sale_folio}' no corresponde al mes de la solicitud.<h5>
-                <br>
-                <button
-                    type=\"button\"
-                    class=\"btn btn-warning\"
-                    onclick=\"close_alert();\"
-                >
-                    <i class=\"icon-cancel-circled\">Cerrar/i>
-                </button>
-                <br>
-            </div>" )));
+            "message"=>"<div class=\"text-center\"><h3 class=\"text-center\">Lo sentimos</h3><h5>Su solicitud ha sido rechazada que que la venta '{$sale_folio}' no corresponde al mes de la solicitud.<h5></div>" )));
             return $response;
         }
         
