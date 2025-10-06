9bfd4a82e2f2dd5d684902fc0e77822784301ad2
neutrix94 <oscar_mdp10@hotmail.com>
2025-10-06 14:32:56 -0600

    Reparacion de paginador de listado de ventas

--- include/adminSales.php
+++ include/adminSales.php
@@ -1,13 +1,17 @@
 <?php
 	session_start();
 	$_SESSION['current_view'] = $_POST['action'];
-	$_SESSION['salesPagination'] = array();//$_POST['action'];
-    $_SESSION['salesPagination']['since'] = ( isset( $_SESSION['salesPagination']['since'] ) ? $_SESSION['salesPagination']['since'] : 1 );
+	//$_SESSION[$_SESSION['current_view']]['salesPagination'] = array();//$_POST['action'];
+    //$_SESSION[$_SESSION['current_view']]['salesPagination']['since'] = ( isset( $_SESSION['salesPagination']['since'] ) ? $_SESSION['salesPagination']['since'] : 1 );
+    //$_SESSION[$_SESSION['current_view']]['salesPagination']['to'] = ( isset( $_SESSION['salesPagination']['to'] ) ? $_SESSION['salesPagination']['to'] : 1 );
     //$_SESSION['salesPagination'][''] =
 	//include('conexion.php');
 	include('./db.php');
+	include('../ajax/SalesDB.php');
 	$db = new db();
 	$link = $db->conectDB();
+	$SalesDB = new SalesDB(connection: $link);
+	$paginator = $SalesDB->getPagesInfo(20, 1);
 //consulta el numero de registros entre el numero de pagina
     $sql = "SELECT
                 COUNT(*) AS pages_limit
@@ -16,19 +20,6 @@
 	$eje=$link->query( $sql )or die("Error al listar las razones sociales : {$sql}");
     $row = $eje->fetch( PDO::FETCH_ASSOC );
     $pages_limit = ROUND( $row['pages_limit'] / 20 );
-	$sql="SELECT 
-            p.id_pedido, 
-            s.nombre AS store_name,
-            p.folio_nv, 
-            p.id_cliente,
-            p.total
-        FROM ec_pedidos p
-        LEFT JOIN sys_sucursales s
-        ON p.id_sucursal = s.id_sucursal
-        WHERE 1 
-        ORDER BY id_pedido DESC
-        LIMIT 20";
-	$eje=$link->query( $sql )or die("Error al listar las razones sociales : {$sql}");
 ?>
 	<div style="width:90%;heigth:450px;">
 		<br>
@@ -54,7 +45,7 @@
             </div>
         </div>
 		<div style="max-height:500px; overflow : auto;">
-			<table width="100%" class="table table-striped">
+			<table width="100%" class="table table-striped table-bordered">
 				<thead class="bg-primary text-light" style="position : sticky; top: 0;">
 					<tr>
 						<th width="20%" class="text-center">Id</th>
@@ -69,56 +60,33 @@
 						<th width="15%" class="text-center">Cliente</th>
 						<th width="15%" class="text-center">Monto</th>
 						<th width="10%" class="text-center">Ver</th>
-						<!--th width="10%" class="text-center">Editar</th>
-						<th width="10%" class="text-center">Eliminar</th-->
 					</tr>
 				</thead>
 				<tbody style="max-height : 200px; overflow:auto;" id="SalesListContent">
-			<?php
-			$c=0;//inicaimos el contador en cero
-			while( $r = $eje->fetch( PDO::FETCH_ASSOC ) ){
-				$c++;//incrementamos contador
-				echo '<tr id="fila_'.$c.'" tabindex="'.$c.'" onfocus="resalta('.$c.');" onclick="resalta('.$c.');" onblur="quita_resaltado('.$c.');">';
-					echo '<td>'.$r['id_pedido'].'</td>';
-					echo '<td>'.$r['store_name'].'</td>';
-					echo '<td>'.$r['folio_nv'].'</td>';
-					echo '<td class="text-center">'.$r['id_cliente'].'</td>';
-					echo '<td>'.$r['total'].'</td>';
-					echo "<td class=\"text-center\">
-						<button
-							type=\"button\"
-							class=\"btn\"
-							onclick=\"saleDetail( {$r['id_pedido']} , 0 );\"
-						>
-							<i class=\"icon-eye\"></i>
-						</button>
-					</td>";
-//				echo '</tr>';
-				echo '</tr>'; 
-			}//fin de while
-			?>
 				</tbody>
 			</table>
 	</div>
     
-    <table class="table">
+    <table class="table" id="paginator_table">
         <tfoot>
             <tr>
                 <th class="text-center">
                     <button
                         class="btn btn-primary"
                         style="box-shadow : 1px 10px 10px rgba( 0,0,0,.4 );"
+						onclick="paginator(-1);"
                     >
                         <i class="icon-left-open"></i>
                     </button>
                 </th>
                 <th class="text-center">
-                    Página <b id="current_page">1</b> de <b id="pages_limit"><?php echo $pages_limit;?></b>
+                    Página <b id="current_page"><?php echo $paginator['current_page'];?></b> de <b id="pages_limit"><?php echo $paginator['pages_counter'];?></b>
                 </th>
                 <th class="text-center">
                     <button
                         class="btn btn-primary"
                         style="box-shadow : 1px 10px 10px rgba( 0,0,0,.4 );"
+						onclick="paginator(1);"
                     >
                         <i class="icon-right-open"></i>
                     </button>
@@ -127,98 +95,8 @@
         </tfoot>
     </table>
 
-	<!--div class="form_emergente" id="emergente_RS" style="display:none;">
-		<div style="position:absolute;top:10%;width:80%;left:10%;">
-			<button class="cierra_emergente" onclick="cierra_emergente('emergente_RS');">X</button>
-				<table width="100%" border="0" cellspacing="10px" cellpadding="10px;" style="background:#B0C4DE;border-radius:15px;">
-					<tr>
-						<td align="right" width="25%"><b class="desc_campo">ID:</b></td>
-						<td align="center" width="25%"><input type="text" id="id_user" class="entrada_txt" disabled></td>
-
-						<td align="right" width="25%"><b class="desc_campo">Activo:</b></td>
-						<td align="center" width="25%"><input type="checkbox" id="estado"></td>					
-					</tr>
-					<tr>
-						<td align="right" width="25%"><b class="desc_campo">Nombre:</b></td>
-						<td align="center" width="25%"><input type="text" id="nombre" class="entrada_txt"></td>
-
-						<td align="right" width="25%"><b class="desc_campo">Login:</b></td>
-						<td align="center" width="25%"><input type="text" id="login" class="entrada_txt"><br></td>
-					</tr>
-					<tr>
-						<td align="right" width="25%"><b class="desc_campo">Password:</b></td>
-						<td align="center" width="25%"><input type="password" id="pass" class="entrada_txt"></td>
-
-						<td align="right" width="25%"><b class="desc_campo">Fecha alta:</b></td>
-						<td align="center" width="25%"><input type="text" id="fecha_alta" class="entrada_txt"></td>
-					</tr>
-					<tr>
-						<td align="right" width="25%"><b class="desc_campo">Tipo de Perfil:</b></td>
-						<td align="center" width="25%"><input type="text" id="perfil" class="entrada_txt"></td>
-
-						<td align="right" width="25%"><b class="desc_campo">Observaciones:</b></td>
-						<td align="center" width="25%"><textarea id="observaciones" class="entrada_txt"></textarea></td>
-						<td></td><td></td>
-					</tr>
-					<tr>
-						<td colspan="4" align="center">
-						<button class="btn_med" onclick="guarda_user();" id="guardar_user">
-								Guardar
-						</button>
-						</td>
-					</tr>
-				</table>
-
-		</div>
-	</div-->
-
 <script>
 var id_rg,nombre,ruta,nom_db,rfc,ruta_link,orden,pss_db,host,user_db,nom_db,estado,obs;
-	/*function guarda_RS(id_reg,flag){
-	//extraemos datos del formulario
-		id_rg=$("#id_razon_social").val();
-		nombre=$("#nombre").val();
-		ruta=$("#ruta").val();
-		nom_db=$("#usuario_db").val();
-		rfc=$("#rfc").val();
-		ruta_link=$("#link").val();
-		orden=$("#orden").val();
-		pss_db=$("#contrasena_db").val();
-		user_db=$("#usuario_db").val();
-		obs=$("#observaciones").val();
-		estado=$("#activo").val();
-	//enviamos datos por ajax
-		$.ajax({
-			type:'post',
-			url:'ajax/rS.php',
-			cache:false,
-			data:{
-					flag:flag,
-					id_registro:id_rg,
-					nom_rs:nombre,
-					server:host,
-					nombre_base_datos:nom_db,
-					rfc:rfc,
-					enlace:ruta_link,
-					ord:orden,
-					pass_base_datos:pss_db,
-					usuario_base_datos:user_db,
-					activo:estado,
-					observ:obs
-				},
-				success:function(dat){
-					var aux=dat.split("|");
-					if(aux[0]!='ok'){
-						alert("Error!!!\n\n"+dat);
-						return false;
-					}else{
-						alert(aux[1]);
-						return true;
-				}
-			}
-		});
-	}*/
-
 	function saleDetail(id,flag){
 	//envia datos por ajax
 		$.ajax({
@@ -233,23 +111,44 @@ var id_rg,nombre,ruta,nom_db,rfc,ruta_link,orden,pss_db,host,user_db,nom_db,esta
 		});//fin de ajax
 	}//fin de funcion que carga datos
 
+	function paginator(action){
+	//consulta pagina actual
+		var current_page = parseInt($('#current_page').html().trim());
+		var pages_limit = parseInt($('#pages_limit').html().trim());
+		current_page += parseInt(action);
+		if(current_page < 1){
+			alert("No hay mas paginas hacia atras.");
+			return false;
+		}else if(current_page > pages_limit){
+			alert("No hay mas paginas hacia adelante.");
+			return false;
+		}
+		getSales( 0, 20, current_page );
+	}
+
 var resaltada=0;
 	function resalta(num){
-		if(resaltada!=0){
+		/*if(resaltada!=0){
 			quita_resaltado(resaltada);
 		}
-		$("#fila_"+num).css("background","rgba(0,225,0,0.5)");
+		$("#fila_"+num).css("background","rgba(0,225,0,0.5)");*/
 	}
 
 	function quita_resaltado(num){
-		$("#fila_"+num).css("background","white");
+		//$("#fila_"+num).css("background","white");
 	}
-    function getSales( start_position = 0, limit = 30 ){
-    //manda a buscar venta
-        var url = `ajax/SalesDB.php?fl=getSales&folio=${text}&start=${start_position}&limit=${limit}`;
-        var resp = ajaxR( url );
+    function getSales( start_position = 0, limit = 20, page = 1){//buscar venta
+        var url = `ajax/SalesDB.php?fl=getPagesSales&limit=${limit}&current_page=${page}`;//&folio=${text}&start=${start_position}
+        var resp = ajaxR( url );//alert(resp);
+		var json = JSON.parse(resp);
+		var content = buildSaleRows(json.sales);
         $( '#SalesListContent' ).html( resp );
-    }
+		
+		$('#SalesListContent').empty();
+		$('#SalesListContent').html(content);
+		$('#current_page').html(json.paginator.current_page);
+	}
+	
     function seekSale( e ){
         var keycode = e.keyCode;
         if( e != 'intro' && keycode != 13 ){
@@ -258,16 +157,45 @@ var resaltada=0;
     //
         var text = $( "#sale_seeker" ).val();
         if( text.length <= 0 ){
-            alert( "Debes ingresar un folio de venta pra buscar." );
+            alert( "Debes ingresar un folio de venta para buscar." );
             $( "#sale_seeker" ).focus();
             return false;
         }
     //manda a buscar venta
         var url = `ajax/SalesDB.php?fl=seekSaleByFolio&folio=${text}`;
-//alert(url);
         var resp = ajaxR( url );
-//alert(resp);
         $( '#SalesListContent' ).html( resp );
-    }
 
+    }
+	function buildSaleRows(json){
+		content = ``;
+		for (const key in json) {
+			content += `<tr id="fila_${key}" tabindex="${key}" onfocus="resalta(${key});" onclick="resalta(${key});" onblur="quita_resaltado(${key});" class="row_item">
+				<td>${json[key]['id_pedido']}</td>
+				<td>${json[key]['store_name']}</td>
+				<td>${json[key]['folio_nv']}</td>
+				<td class="text-center">${json[key]['id_cliente']}</td>
+				<td>${json[key]['total']}</td>
+				<td class="text-center">
+					<button
+						type="button"
+						class="btn"
+						onclick="saleDetail( ${json[key]['id_pedido']} , 0 );"
+					>
+						<i class="icon-eye"></i>
+					</button>
+				</td>
+			</tr>`;
+		}
+		return content;
+	}
+</script>
+<style>
+	.row_item:hover{
+		background-color: rgba(225,0,0,.5) !important;
+	}
+</style>
+
+<script>
+	getSales( 1, 20 );
 </script>
