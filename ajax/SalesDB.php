e4a77234f68dd38cdb250f1a29c0617040226dd2
neutrix94 <oscar_mdp10@hotmail.com>
2025-10-07 20:23:19 -0600

    CAMBIAR A COLOR VERDE EL COLOR DE LA FILA EN LA PANTALLA DE VENTAS CON ERROR AL ENVIAR A RS  LAS QUE YA FUERON INSERTADAS EN LA RAZON SOCIAL 1.1

--- ajax/SalesDB.php
+++ ajax/SalesDB.php
@@ -13,8 +13,6 @@
 
             case 'getPagesSales' : 
                 $limit = ( isset( $_POST['limit'] ) ? $_POST['limit'] : (isset($_GET['limit']) ? $_GET['limit'] : 20) );
-                $since = ( isset( $_POST['since'] ) ? $_POST['since'] : (isset($_GET['since']) ? $_GET['since'] : 0) );
-                $to = ( isset( $_POST['to'] ) ? $_POST['to'] : (isset($_GET['to']) ? $_GET['to'] : 20) );
                 $current_page = ( isset( $_POST['current_page'] ) ? $_POST['current_page'] : (isset($_GET['current_page']) ? $_GET['current_page'] : 0) );
                 echo json_encode($SalesDB->getPagesSales( $limit, $current_page));
             break;
